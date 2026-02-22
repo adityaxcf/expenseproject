40d493c2b2eb1987236cf34ece5192a807f00a16
Aditya J <adityaj@Adityas-MacBook-Air-3.local>
2026-02-22 14:26:39 +0530

    Fixed file casing and cleaned expense model imports

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const Expense = require("../models/Expense");
+const Expense = require("../models/expense");
 
 // Test route
 router.get("/test", (req, res) => {
